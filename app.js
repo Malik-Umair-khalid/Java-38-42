8ea03e18913cb184413ad74d992742c0e3f38233
Malik Umair Khalid <fighteromega3462@gmail.com>
2021-06-06 16:34:21 +0500

    Question 4 completed

---
+++ app.js
@@ -0,0 +1,52 @@
+// //Q1
+
+// function powerCalc (number, power){
+// var calc = number**power
+//   alert (calc)
+// }
+// var number = prompt("Write The number")
+// var power = prompt("Write The power")
+// powerCalc(number, power)
+
+// // Q 2
+
+// function leepYearDectect(year){
+//     var flag = false;
+//     if(year % 400 == 0){
+//         flag = true;
+//     }
+//     else if (year % 100 == 0){
+//         flag = false;
+//     }
+//     else if (year % 4 == 0){
+//         flag = true;
+//     }
+//     else{
+//         flag = false;
+//     }
+//     if (flag){
+//     alert("year " + year + "is a leep Year")
+// }
+// else{
+//     alert("year " + year + "is not a leep Year")
+// }
+// }
+// var year = prompt("Write The Year")
+// leepYearDectect(year)
+
+// // Q 3
+
+// function areaOfTriangle(sideA,sideB,sideC){
+//     var side = (sideA + sideB + sideC) / 2
+//     var areaA = (side - sideA)
+//     var areaB = (side - sideB)
+//     var areaC = (side - sideC)
+//     var area = side * areaA * areaB * areaC
+//     alert(area) 
+// }
+// var a = +prompt("Enter Value of Side A")
+// var b = +prompt("Enter Value of Side B")
+// var c = +prompt("Enter Value of Side C")
+// areaOfTriangle(a,b,c)
+
+// Q 4
